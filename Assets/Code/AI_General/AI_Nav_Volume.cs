097e5ed1fe0b816018d1c23a46a180d9620f7d4d
Miss Jade Elizabeth Winters <jadewinters.personal.2024@gmail.com>
2025-10-10 17:41:58 +0100

    added func to get point within volume

--- Assets/Code/AI_General/AI_Nav_Volume.cs
+++ Assets/Code/AI_General/AI_Nav_Volume.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 /*
  * TODO: Use provided data from unity framework for now until we have our own
  */
@@ -14,15 +15,25 @@ public class AI_Nav_Volume : MonoBehaviour
     public float Height = 20.0f;
     public float Depth = 20.0f;
 
-    void OnStart()
+    public Vector3 GetRandomPointWithinVolume()
     {
+        return new Vector3(
+                    transform.position.x + Random.Range(-Width / 2f, Width / 2f + 1.0f),
+                    transform.position.y + Random.Range(-Height / 2f, Height / 2f + 1.0f),
+                    transform.position.z + Random.Range(-Depth / 2f, Depth / 2f + 1.0f)
+                );
+    }
+
+    void Start()
+    {
+        Random.InitState(0);
         transform.position = Vector3.zero;
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
-        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height, Depth)); 
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height, Depth));
     }
 
     void OnDrawGizmosSelected()
