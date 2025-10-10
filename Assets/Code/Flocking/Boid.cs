1f35fa0ee13de968ee16830e68c3c78023f09c4b
Miss Jade Elizabeth Winters <jadewinters.personal.2024@gmail.com>
2025-10-10 18:53:44 +0100

    working on setup

--- Assets/Code/Flocking/Boid.cs
+++ Assets/Code/Flocking/Boid.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector3 m_position = Vector3.zero;
+    public Vector3 m_velocity = Vector3.zero;
+
+
+    private GameObject m_game_object;
+    private Vector3 m_scale = Vector3.one * 0.7f;
+
     void Start()
     {
-        
+        m_game_object = GameObject.CreatePrimitive(PrimitiveType.Sphere);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        m_position += m_velocity * Time.deltaTime;
+        transform.position = m_position;
+
+        if (m_game_object != null)
+        {
+            m_game_object.transform.position = m_position; ;
+            m_game_object.transform.localScale = m_scale;
+            m_game_object.transform.SetParent(transform);
+        }
     }
 }
--- Assets/Code/Flocking/FlockingController.cs
+++ Assets/Code/Flocking/FlockingController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class FlockingController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Boid Meta")]
+    public int NumberOfBoids = 10;
+
+    private GameObject[] m_boids;
+
+    private float m_AlignmentWeight = 0;
+    private float m_CohesionWeight = 0;
+    private float m_AvoidanceWeight = 0;
+
+
     void Start()
     {
-        
+        AI_Nav_Volume NavVolume = FindObjectOfType<AI_Nav_Volume>();
+        if (NavVolume == null)
+            return;
+
+        m_boids = new GameObject[NumberOfBoids];
+
+        for(int i = 0; i < m_boids.Length; i++)
+        {
+            m_boids[i] = new GameObject("Boid_" + i);
+            Boid boid = m_boids[i].AddComponent<Boid>();
+            boid.m_position = NavVolume.GetRandomPointWithinVolume();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CreateSlider slider = FindObjectOfType<CreateSlider>();
+        if (slider == null)
+            return;
+
+        m_CohesionWeight = (float)System.Math.Round((float)slider.m_cohesion_slider.value / 100.0f, 2);
+        m_AlignmentWeight = (float)System.Math.Round((float)slider.m_alignment_slider.value / 100.0f, 2);
+        m_AvoidanceWeight = (float)System.Math.Round((float)slider.m_avoidance_slider.value / 100.0f, 2);
+
+        for(int i = 0; i < m_boids.Length; i++)
+        {
+            //continue with rules here
+        }
     }
+
+
+
 }
