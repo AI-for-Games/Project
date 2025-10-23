8d413b49392ca49dc215261d333277db13810e10
Miss Jade Elizabeth Winters <jadewinters.personal.2024@gmail.com>
2025-10-23 01:59:54 +0100

    Added some comments, removed redundant code, added screen space control of boids, fixed warnings

--- Assets/Code/Flocking/Boid.cs
+++ Assets/Code/Flocking/Boid.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
+    //We define our own position and velocity for more control
     public Vector3 m_position = Vector3.zero;
     public Vector3 m_velocity = Vector3.one;
+
+    //Neighboring boids queerying data
     public float m_neighbor_search_radius = 1000.0f;
     public int m_max_number_of_neighbors = 1000;
 
+    //visual representation 
     private GameObject m_game_object;
     private Vector3 m_scale = Vector3.one * 0.7f;
 
+    //Create the visual representation
     void Start()
     {
         m_game_object = GameObject.CreatePrimitive(PrimitiveType.Sphere);
@@ -19,9 +24,11 @@ public class Boid : MonoBehaviour
 
     void Update()
     {
+        //update position using velocity relative to dt, and have this reflect in the owner 
         m_position += m_velocity * Time.deltaTime;
         transform.position = m_position;
 
+        //do the same for the visual representation -> see comment above 
         if (m_game_object != null)
         {
             m_game_object.transform.position = m_position; ;
--- Assets/Code/Flocking/FlockingController.cs
+++ Assets/Code/Flocking/FlockingController.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class FlockingController : MonoBehaviour
 {
+    //expose boid count to editor ui
     [Header("Boid Meta")]
     public int NumberOfBoids = 1000;
 
+    //array of  boids
     private GameObject[] m_boids;
 
+    //flocking rule weights, retrieved from custom ui sliders
     private float m_AlignmentWeight = 0;
     private float m_CohesionWeight = 0;
     private float m_AvoidanceWeight = 0;
 
     void Start()
     {
+        //grab the nav volume from the scene
         AI_Nav_Volume NavVolume = FindObjectOfType<AI_Nav_Volume>();
         if (NavVolume == null)
             return;
 
+        //create the boid array
         m_boids = new GameObject[NumberOfBoids];
 
+        //create, configure and assign boid to boid array
         for (int i = 0; i < m_boids.Length; i++)
         {
             m_boids[i] = new GameObject("Boid_" + i);
@@ -32,22 +38,34 @@ public class FlockingController : MonoBehaviour
     /*
      * This can be substituted for spacial partitioning later on
      */
-    List<int> GetNeighborsOfBoid(int index)
+    List<int> GetNeighborsOfBoid(int Index)
     {
+        //define empty list for neighbors
         List<int> Neighbors = new List<int>();
 
-        Boid local_boid = m_boids[index].GetComponent<Boid>();
+        //grab local boid
+        Boid LocalBoid = m_boids[Index].GetComponent<Boid>();
 
-        for (int i = 0; i < m_boids.Length; i++)
+        //loop over all boids
+        for (int I = 0; I < m_boids.Length; I++)
         {
-            if (i == index) continue;
+            //check current boid is not the local boid
+            if (I == Index) continue;
 
-            Boid possible_neighbor = m_boids[i].GetComponent<Boid>();
+            //grab neighbor boid position
+            Vector3 PossibleNeighborPossition = m_boids[I].GetComponent<Boid>().m_position;
 
-            if ((local_boid.m_position - possible_neighbor.m_position).sqrMagnitude < local_boid.m_neighbor_search_radius * local_boid.m_neighbor_search_radius)
+            //define radius sqrd of local boid search radius
+            float SearchRadiusSqrd = LocalBoid.m_neighbor_search_radius * LocalBoid.m_neighbor_search_radius;
+
+            //check if distance between two boids are within range
+            if ((PossibleNeighborPossition - LocalBoid.m_position).sqrMagnitude < SearchRadiusSqrd)
             {
-                Neighbors.Add(i);
-                if (Neighbors.Count >= local_boid.m_max_number_of_neighbors)
+                //add neighbor to neighbor array
+                Neighbors.Add(I);
+                
+                //check if max neighbor count is reached
+                if (Neighbors.Count >= LocalBoid.m_max_number_of_neighbors)
                     break;
             }
         }
@@ -57,49 +75,69 @@ public class FlockingController : MonoBehaviour
 
     Vector3 GetAlignmentContribution(int Index, List<int> Neighbors)
     {
+        //check for empty neighbors
         if (Neighbors.Count == 0) return Vector3.zero;
 
+        //define empty resultant
         Vector3 Resultant = Vector3.zero;
 
-        for (int i = 0; i < Neighbors.Count; i++)
+        //loop over neighbors and sum the velocities
+        for (int I = 0; I < Neighbors.Count; I++)
         {
-            Boid neighbor_boid = m_boids[Neighbors[i]].GetComponent<Boid>();
+            Boid neighbor_boid = m_boids[Neighbors[I]].GetComponent<Boid>();
             Resultant += neighbor_boid.m_velocity;
         }
-        return Resultant / Neighbors.Count * m_AlignmentWeight;
+        //return the average velocity normaized into the alignment weight
+        return (Resultant / Neighbors.Count).normalized * m_AlignmentWeight;
+    }
+
+    Vector3 GetAveragePositionOfNeighboringBoids(List<int> Neighbors)
+    {
+        //define empty resultant
+        Vector3 Resultant = Vector3.zero;
+
+        //loop over all neighbors and add position to resultant
+        for (int i = 0; i < Neighbors.Count; i++)
+        {
+            Resultant += m_boids[Neighbors[i]].GetComponent<Boid>().m_position;
+        }
+        return Resultant / Neighbors.Count;
     }
 
     Vector3 GetAvoidanceContribution(int Index, List<int> Neighbors)
     {
+        //check for empty neighbors
         if (Neighbors.Count == 0) return Vector3.zero;
 
-        Vector3 Resultant = Vector3.zero;
+        //grab average position of neighbor boids
+        Vector3 ToNeighboringBoids = GetAveragePositionOfNeighboringBoids(Neighbors);
 
-        for (int i = 0; i < Neighbors.Count; i++)
-        {
-            Boid neighbor_boid = m_boids[Neighbors[i]].GetComponent<Boid>();
-            Resultant += neighbor_boid.m_position - m_boids[Index].GetComponent<Boid>().m_position;
-        }
-        return Resultant / Neighbors.Count * -m_AvoidanceWeight;
+        //grab local boid position
+        Vector3 LocalBoidPosition = m_boids[Index].GetComponent<Boid>().m_position;
+
+        //compute resultant
+        return -(ToNeighboringBoids - LocalBoidPosition).normalized * m_AvoidanceWeight;
     }
 
     Vector3 GetCohesionContribution(int Index, List<int> Neighbors)
     {
+        //check for empty neighbors
         if (Neighbors.Count == 0) return Vector3.zero;
 
-        Vector3 Resultant = Vector3.zero;
+        //grab average position of neighbor boids
+        Vector3 ToNeighboringBoids = GetAveragePositionOfNeighboringBoids(Neighbors);
 
-        for (int i = 0; i < Neighbors.Count; i++)
-        {
-            Boid neighbor_boid = m_boids[Neighbors[i]].GetComponent<Boid>();
-            Resultant += neighbor_boid.m_position;
-        }
-        return ((Resultant / Neighbors.Count)-m_boids[Index].GetComponent<Boid>().m_position) * m_CohesionWeight;
+        //grab local boid position
+        Vector3 LocalBoidPosition = m_boids[Index].GetComponent<Boid>().m_position;
+
+        //compute resultant
+        return (ToNeighboringBoids - LocalBoidPosition).normalized * m_CohesionWeight;
     }
 
 
     void Update()
     {
+        //update flocking rule coefficients
         CreateSlider slider = FindObjectOfType<CreateSlider>();
         if (slider == null)
             return;
@@ -108,14 +146,41 @@ public class FlockingController : MonoBehaviour
         m_AlignmentWeight = (float)System.Math.Round((float)slider.m_alignment_slider.value / 100.0f, 2);
         m_AvoidanceWeight = (float)System.Math.Round((float)slider.m_avoidance_slider.value / 100.0f, 2);
 
+        //for each boid
         for (int i = 0; i < m_boids.Length; i++)
         {
+            //grab all neighbors
             List<int> Neighbors = GetNeighborsOfBoid(i);
-            Vector3 Resultant = GetAlignmentContribution(i, Neighbors) +GetCohesionContribution(i, Neighbors) +GetAvoidanceContribution(i, Neighbors);
+
+            //compute final resultant
+            Vector3 Resultant = Vector3.zero;
+            Resultant += GetAlignmentContribution(i, Neighbors);
+            Resultant += GetCohesionContribution(i, Neighbors);
+            Resultant += GetAvoidanceContribution(i, Neighbors);
 
             //add some randomness
-            Resultant += new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10)).normalized *0.5f;
-            m_boids[i].GetComponent<Boid>().m_velocity = Resultant;
+            Vector3 RandomVec3 = new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10)).normalized;
+            RandomVec3.y = 0.0f;
+            RandomVec3.z = 0.0f;
+            Resultant += RandomVec3 * 0.5f;
+
+            //little rule to keep boids draw to centre of scene
+            //Resultant += (Vector3.zero - m_boids[i].GetComponent<Boid>().m_position) * 0.4f;
+
+            //test code
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 boidPos = m_boids[i].GetComponent<Boid>().m_position;
+                Vector3 ToHitPoint = hit.point - boidPos;
+                ToHitPoint.y = 0.0f;
+                Resultant += ToHitPoint.normalized * 0.4f;
+            }
+            //test code
+
+            //set boid velocity
+            m_boids[i].GetComponent<Boid>().m_velocity = Resultant * (Random.Range(1, 5));
         }
     }
 
