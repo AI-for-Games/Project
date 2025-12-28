ba776bba5a61aee3176712794d38b337580c3f05
Anth <89168209+breegus@users.noreply.github.com>
2025-12-28 23:59:47 +0000

    additive gen modified to handle 1x3 walls (not done!)

--- Assets/Code/Generation/DungeonGenerator.cs
+++ Assets/Code/Generation/DungeonGenerator.cs
@@ -13,15 +13,44 @@ public class DungeonGenerator : MonoBehaviour
 {
     public GameObject floorPrefab;
 
-    public GameObject wallNorth;
-    public GameObject wallSouth;
-    public GameObject wallEast;
-    public GameObject wallWest;
+    public GameObject[] wallPreFabs;
 
     public int width = 50;
     public int height = 50;
 
+    public float tileSize = 1f;
+    public float wallHalfLength = 1.5f;
+    public float wallThicknessOffset = 0.5f;
+
     private CellType[,] grid;
+    private bool[,] xWalls;
+    private bool[,] yWalls;
+
+    private Quaternion RotNorth = Quaternion.Euler(0, 0, 0);
+    private Quaternion RotEast = Quaternion.Euler(0, 90, 0);
+    private Quaternion RotSouth = Quaternion.Euler(0, 180, 0);
+    private Quaternion RotWest = Quaternion.Euler(0, 270, 0);
+
+    private Vector3 OffsetNorth = new Vector3(0, 0, 0);
+    private Vector3 OffsetSouth = new Vector3(0, 0, 0);
+    private Vector3 OffsetEast = new Vector3(0, 0, 0);
+    private Vector3 OffsetWest = new Vector3(0, 0, 0);
+
+    void Start()
+    {
+        OffsetNorth = new Vector3(0, 0, wallThicknessOffset);
+        OffsetSouth = new Vector3(0, 0, -wallThicknessOffset);
+        OffsetEast = new Vector3(wallThicknessOffset, 0, 0);
+        OffsetWest = new Vector3(-wallThicknessOffset, 0, 0);
+
+        Generate();
+
+        SpawnFloors();
+
+        BuildWallMaps();
+        SpawnXWalls();
+        SpawnYWalls();
+    }
 
     void Generate()
     {
@@ -31,8 +60,8 @@ public class DungeonGenerator : MonoBehaviour
 
         for (int i = 0; i < roomCount; i++)
         {
-            int roomW = Random.Range(4, 8);
-            int roomH = Random.Range(4, 8);
+            int roomW = Random.Range(6, 10);
+            int roomH = Random.Range(6, 10);
             int x = Random.Range(1, width - roomW - 1);
             int y = Random.Range(1, width - roomH - 1);
 
@@ -43,6 +72,110 @@ public class DungeonGenerator : MonoBehaviour
                     grid[rx, ry] = CellType.Floor;
                 }
             }
+        }
+    }
+
+    void BuildWallMaps()
+    {
+        xWalls = new bool[width, height];
+        yWalls = new bool[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (!IsFloor(x, y)) continue;
+
+                if (!IsFloor(x, y + 1))
+                    yWalls[x, y] = true;
+
+                if (!IsFloor(x + 1, y))
+                    xWalls[x, y] = true;
+            }
+        }
+    }
+
+    void SpawnXWalls()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int runStart = -1;
+
+            for (int y = 0; y <= height; y++)
+            {
+                bool hasWall = (y < height) && xWalls[x, y];
+
+                if (hasWall && runStart == -1)
+                {
+                    runStart = y;
+                }
+                else if (!hasWall && runStart != -1)
+                {
+                    SpawnXWall(x, runStart, y - 1);
+                    runStart = -1;
+                }
+            }
+        }
+    }
+
+    void SpawnYWalls()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            int runStart = -1;
+
+            for (int x = 0; x <= width; x++)
+            {
+                bool hasWall = (x < width) && yWalls[x, y];
+
+                if (hasWall && runStart == -1)
+                {
+                    runStart = x;
+                }
+                else if (!hasWall && runStart != -1)
+                {
+                    SpawnYWall(runStart, x - 1, y);
+                    runStart = -1;
+                }
+            }
+        }
+    }
+
+    void SpawnXWall(int startX, int endX, int y)
+    {
+        int length = endX - startX + 1;
+        int wallCount = length / 3;
+
+        for (int i = 0; i < wallCount; i++)
+        {
+            int x = startX + i * 3 + 1; // center of 3 tiles
+
+            Vector3 pos = new Vector3(
+                x * tileSize,
+                0,
+                (y * tileSize) + wallThicknessOffset
+            );
+
+            Instantiate(GetRandWall(), pos, RotNorth, transform);
+        }
+    }
+
+    void SpawnYWall(int x, int startY, int endY)
+    {
+        int length = endY - startY + 1;
+        int wallCount = length / 3;
+
+        for (int i = 0; i < wallCount; i++)
+        {
+            int y = startY + i * 3 + 1;
+
+            Vector3 pos = new Vector3(
+                (x * tileSize) - wallThicknessOffset,
+                0,
+                y * tileSize
+            );
+
+            Instantiate(GetRandWall(), pos, RotEast, transform);
         }
     }
 
@@ -65,23 +198,26 @@ public class DungeonGenerator : MonoBehaviour
         }
     }
 
-    //bool IsFloor(int x, int y)
-    //{
-    //    if (x < 0 || y < 0 || x >= width || y >= height)
-    //        return false;
-    //
-    //    return grid[x, y] == CellType.Floor;
-    //}
+    GameObject GetRandWall()
+    {
+        return wallPreFabs[Random.Range(0, wallPreFabs.Length)];
+    }
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    
-    //}
+    void SpawnWall(Vector3 position, Quaternion rotation)
+    {
+        Instantiate(GetRandWall(), position, rotation, transform);
+    }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    
-    //}
+    Vector3 GridToWorld(int x, int y)
+    {
+        return new Vector3(x * tileSize, 0, y * tileSize);
+    }
+
+    bool IsFloor(int x, int y)
+    {
+        if (x < 0 || y < 0 || x >= width || y >= height)
+            return false;
+    
+        return grid[x, y] == CellType.Floor;
+    }
 }
